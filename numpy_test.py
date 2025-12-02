3e8c0f0487bfaea53f2434f707b621507edb30d9
ayush-shrestha-codes <aayushshresthaa123@gmail.com>
2025-12-02 14:45:55 +0545

    updated

--- numpy_test.py
+++ numpy_test.py
@@ -3,10 +3,10 @@ import numpy as np
 
 df = pd.read_csv("data/covid.csv")
 
-#print(df.head())
-#print(df.isnull().sum())
+print(df.head())
+print(df.isnull().sum())
 
-#df = df[['continent', 'Date', 'Deaths', 'Recovered']]
-#print(df.head())
+df = df[['continent', 'Date', 'Deaths', 'Recovered']]
+print(df.head())
 print(df.columns)
 df.rename(columns={'date': 'Date', }, inplace=True)
