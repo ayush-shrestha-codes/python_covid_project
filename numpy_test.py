aa0481687d9cb23b7682c66d4bf386a7a2a0df94
ayush-shrestha-codes <aayushshresthaa123@gmail.com>
2025-12-02 18:53:17 +0545

    montly calculation performed

--- numpy_test.py
+++ numpy_test.py
@@ -28,3 +28,22 @@ cases_by_location['death_ratio'] = np.where(
 cases_by_location['death_ratio'] = cases_by_location['death_ratio'].round(2)#for simplicity
 top10_death_ratio = cases_by_location.sort_values(by='death_ratio', ascending=False).head(10)
 print(top10_death_ratio[['location','total_cases','total_deaths','death_ratio']])
+
+#monthly trends
+df['year_month'] = df['date'].dt.to_period('M')
+print(df['year_month'])
+monthly_trends = df.groupby('year_month')[['new_cases','new_deaths']].sum().reset_index()
+top10_months = monthly_trends.head(10)
+print(top10_months)
+
+#peak montly cases 
+
+# Peak month by new cases
+peak_cases_month = monthly_trends.loc[monthly_trends['new_cases'].idxmax()]
+print("Peak month by cases:")
+print(peak_cases_month)
+
+# Peak month by new deaths
+peak_deaths_month = monthly_trends.loc[monthly_trends['new_deaths'].idxmax()]
+print("Peak month by deaths:")
+print(peak_deaths_month)
