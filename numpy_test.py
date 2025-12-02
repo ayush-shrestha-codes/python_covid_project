8d782d86797da5d1d98e1caa57f861065deb6302
ayush-shrestha-codes <aayushshresthaa123@gmail.com>
2025-12-02 14:37:19 +0545

    Initial commit

---
+++ numpy_test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("data/covid.csv")
+
+#print(df.head())
+#print(df.isnull().sum())
+
+#df = df[['continent', 'Date', 'Deaths', 'Recovered']]
+#print(df.head())
+print(df.columns)
+df.rename(columns={'date': 'Date', }, inplace=True)
---
+++ test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'Diana', 'Ethan', 'Fiona', 'George', 'Hannah'],
+    'Age': [25, 30, 28, 35, 26, 35, 30, 25],
+    'Salary': [55000, 62000, 58000, 72000, 54000, 68000, 60000, 57000],
+    'Rating': [4.5, 4.0, 3.8, 4.7, 4.2, 4.1, 3.9, 4.6]
+}
+
+df = pd.DataFrame(data)
+grouped=df.groupby(["Age","Name"])["Salary"].mean()
+print(grouped)
